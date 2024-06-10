d67d32ba4192eecea2bd3eea1fa9967583940d32
Sridhar1030 <77533416+Sridhar1030@users.noreply.github.com>
2024-06-10 18:56:33 +0530

    global objects

---
+++ Basics/global.js
@@ -0,0 +1,27 @@
+
+// __dirname: The directory name of the current module
+console.log('__dirname:', __dirname);
+
+// __filename: The file name of the current module
+console.log('__filename:', __filename);
+
+// process: Information about the current Node.js process
+console.log('process:', process);
+
+
+
+// process.env: Access environment variables.
+console.log('process.env:', process.env);
+
+// process.argv: Command line arguments.
+console.log('process.argv:', process.argv);
+
+// process.exit(): Exiting the current process
+
+// process.exit(1);
+
+// process.cwd(): Current working directory
+console.log('process.cwd():', process.cwd());
+
+// process.memoryUsage(): Memory usage of the Node.js process.
+console.log('process.memoryUsage():', process.memoryUsage());
