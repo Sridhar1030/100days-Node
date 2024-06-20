bb3efacc69cba8623a7681090c8fa1534d9f3849
Sridhar1030 <77533416+Sridhar1030@users.noreply.github.com>
2024-06-21 01:30:29 +0530

    Day 12

---
+++ Express/QueryParameters.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const port = 4000;
+
+
+// Route that handles query parameters
+app.get("/search", (req, res) => {
+  const query = req.query;
+  res.send(`Query parameters received: ${JSON.stringify(query)}`);
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
