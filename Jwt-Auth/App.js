44caf124d2358af69aac26442bd6c4a8e43826aa
Sridhar1030 <77533416+Sridhar1030@users.noreply.github.com>
2024-06-23 22:17:16 +0530

    simple JWT login

---
+++ Jwt-Auth/App.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+const PORT = 3000;
+const SECRET_KEY = 'your_secret_key';
+app.use(cors());
+app.use(bodyParser.json());
+
+// Middleware to protect routes
+const authenticateJWT = (req, res, next) => {
+    const token = req.header('Authorization')?.split(' ')[1];
+
+    if (token) {
+        jwt.verify(token, SECRET_KEY, (err, user) => {
+            if (err) {
+                return res.sendStatus(403);
+            }
+            req.user = user;
+            next();
+        });
+    } else {
+        res.sendStatus(401);
+    }
+};
+
+
+// Sample users data
+const users = [
+    { username: 'sridhar', password: 'newPassword' },
+    { username: 'XYZ', password: 'password2' },
+    { username: 'ABC', password: 'password3' },
+];
+
+// Login route
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+
+    const user = users.find(u => u.username === username && u.password === password);
+
+    if (user) {
+        const accessToken = jwt.sign({ username: user.username }, SECRET_KEY, { expiresIn: '1h' });
+        res.json({ accessToken });
+    } else {
+        res.sendStatus(401);
+    }
+});
+
+// Protected route
+app.get('/protected', authenticateJWT, (req, res) => {
+    res.json({ message: 'This is a protected route', user: req.user });
+    res.json({message:'hello'});
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
