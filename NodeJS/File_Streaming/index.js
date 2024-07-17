6e219b74b8280c9ef162c24c4bd2aab9f2b53b51
Sridhar1030 <77533416+Sridhar1030@users.noreply.github.com>
2024-07-17 21:10:11 +0530

    Day 38

---
+++ NodeJS/File_Streaming/index.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import fs from 'fs';
+import path from 'path';
+
+const app = express();
+
+// Define a route to stream a file
+app.get('/download', (req, res) => {
+    // const filePath = 'F:/100days-Node/NodeJS/File_Streaming/Syllabus.pdf'; 
+    const filePath = 'F:/100days-Node/NodeJS/File_Streaming/example.txt'; 
+    
+    console.log('filePath:', filePath);
+    
+    // Ensure the file exists
+    fs.stat(filePath, (err, stats) => {
+        if (err) {
+            console.error('File not found:', err);
+            return res.status(404).end();
+        }
+        
+        // Set appropriate headers
+        res.writeHead(200, {
+            'Content-Type': 'text/plain',
+            'Content-Length': stats.size
+        });
+        
+        // Create a readable stream from the file and pipe it to the response
+        const readableStream = fs.createReadStream(filePath);
+        readableStream.pipe(res);
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
