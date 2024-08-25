1d3a448c6aba2f1c7a0cdd8d226e5ac5331cee73
Sridhar1030 <77533416+Sridhar1030@users.noreply.github.com>
2024-08-25 20:06:55 +0530

    Day 77

--- NodeJS/BlackList/src/routes/auth.js
+++ NodeJS/BlackList/src/routes/auth.js
@@ -3,6 +3,7 @@ import { blacklistToken } from "../middlewares/blacklist.js";
 import { authenticate } from "../middlewares/authenticate.js";
 import signToken from "../utils/jwt.js"; // Adjust the path as needed
 import User from "../models/user.js"; // Assuming you have a User model
+import BlacklistedToken from "../models/BlacklistedToken.js";
 
 const router = express.Router();
 
@@ -17,17 +18,16 @@ router.post("/login", async (req, res) => {
 			.status(401)
 			.json({ message: "Invalid username or password" });
 	}
+
 	// Check if the user is blacklisted
-	const blacklistedToken = await BlacklistedToken.findOne({
-		token: req.headers.authorization.split(" ")[1],
-	});
+	const token = signToken(user._id);
+	const blacklistedToken = await BlacklistedToken.findOne({ token });
+
 	if (blacklistedToken) {
 		return res.status(401).json({ message: "Token has been blacklisted" });
 	}
 
-	const token = signToken({ id: user._id });
-
-	res.status(200).json({token});
+	res.status(200).json({ token });
 });
 
 router.post("/logout", authenticate, blacklistToken);
