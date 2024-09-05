a7e2d6ede3a5945a8980e24db625dc4904b2ffe8
Sridhar1030 <77533416+Sridhar1030@users.noreply.github.com>
2024-09-05 20:23:16 +0530

    Day 88

--- NodeJS/Jwt-Auth/App.js
+++ NodeJS/Jwt-Auth/App.js
@@ -2,31 +2,33 @@ const express = require("express");
 const jwt = require("jsonwebtoken");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const { jwtDecrypt, jwtEncrypt, generateKeyPair } = require("jose");
+
 const app = express();
 const PORT = 3000;
 const SECRET_KEY = "your_secret_key";
 
-
-
-
+// Generate RSA key pair for encryption
+const { publicKey, privateKey } = generateKeyPair("RSA-OAEP");
 
 app.use(cors());
 app.use(bodyParser.json());
 
 // Middleware to protect routes
-const authenticateJWT = (req, res, next) => {
+const authenticateJWT = async (req, res, next) => {
 	const token = req.header("Authorization")?.split(" ")[1];
 
 	if (token) {
-		jwt.verify(token, SECRET_KEY, (err, user) => {   
-			if (err) {
-				return res.sendStatus(403);
-			}
-			req.user = user;
+		try {
+			const decryptedToken = await jwtDecrypt(token, privateKey); // Decrypt token
+			const decoded = jwt.verify(decryptedToken.payload, SECRET_KEY);
+			req.user = decoded;
 			next();
-		});
+		} catch (err) {
+			return res.sendStatus(403); // Invalid token
+		}
 	} else {
-		res.sendStatus(401);
+		res.sendStatus(401); // No token provided
 	}
 };
 
@@ -35,11 +37,11 @@ const users = [
 	{ username: "sridhar", password: "newPassword" },
 	{ username: "XYZ", password: "password2" },
 	{ username: "ABC", password: "password3" },
-    { username: "DEF", password: "password4" },
+	{ username: "DEF", password: "password4" },
 ];
 
 // Login route
-app.post("/login", (req, res) => {
+app.post("/login", async (req, res) => {
 	const { username, password } = req.body;
 
 	const user = users.find(
@@ -47,19 +49,21 @@ app.post("/login", (req, res) => {
 	);
 
 	if (user) {
-		const accessToken = jwt.sign({ username: user.username }, SECRET_KEY, { //JWT sign makes the payload in jwt string payload
-			expiresIn: "1h",
-		});
-		res.json({ accessToken });
+		const payload = { username: user.username };
+		const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "1h" });
+
+		// Encrypt the JWT token
+		const encryptedToken = await jwtEncrypt(token, publicKey);
+
+		res.json({ accessToken: encryptedToken });
 	} else {
-		res.sendStatus(401);
+		res.sendStatus(401); // Invalid credentials
 	}
 });
 
 // Protected route
 app.get("/protected", authenticateJWT, (req, res) => {
 	res.json({ message: "This is a protected route", user: req.user });
-	res.json({ message: "hello" });
 });
 
 app.listen(PORT, () => {
